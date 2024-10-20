5cc63b41ed24ddb171a6f402c9837247e56f87fe
BisanGhoul <bbisann@gmail.com>
2024-10-20 11:59:01 +0300

    add product array mockup

---
+++ src/utils/product-data.js
@@ -0,0 +1,41 @@
+export const products = [
+    new Product(
+      1001, 
+      'Men’s Black Shirt', 
+      29.99, 
+      100, 
+      'Men', 
+      'Shirt', 
+      'Black', 
+      'Medium', 
+      'A stylish black shirt for men.', 
+      ['https://image1.jpg', 'https://image2.jpg'], 
+      ['new', 'bestseller']
+    ),
+    new Product(
+      1002, 
+      'Women’s Red Dress', 
+      49.99, 
+      50, 
+      'Women', 
+      'Dress', 
+      'Red', 
+      'Small', 
+      'A beautiful red dress.', 
+      ['https://image3.jpg', 'https://image4.jpg'], 
+      ['sale', 'summer']
+    ),
+    new Product(
+      1003, 
+      'Children’s Blue Pants', 
+      19.99, 
+      200, 
+      'Children', 
+      'Pants', 
+      'Blue', 
+      'Large', 
+      'Comfortable blue pants for kids.', 
+      ['https://image5.jpg'], 
+      ['new']
+    ),
+  ];
